5b1d5a8359668f6c362b7701654e45e76da2e490
Farhan Ahmed <iamfarhan09@gmail.com>
2023-05-23 16:35:50 +0600

    My Future loaded

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Link from "next/link";
+
+const Header = () => {
+	return (
+		<nav style={{textAlign:'center'}}>
+			<h1> Nav Bar</h1>
+		</nav>
+	);
+};
+
+export default Header;
